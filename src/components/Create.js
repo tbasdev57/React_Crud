51564aa14a5387db3569e46a423d5a63114072db
tbasdev57 <basbunartayyib@gmail.com>
2024-06-29 19:28:04 +0200

    added initial project files

---
+++ src/components/Create.js
@@ -0,0 +1,47 @@
+import React, { useState, useContext } from "react";
+import firebase from "../utils/firebaseConfig";
+import { UidContext } from "./UidContext";
+
+const Create = () => {
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+
+  const uid = useContext(UidContext);
+
+  const createQuote = () => {
+    const quotesDB = firebase.database().ref("quotesDB");
+    const quote = {
+      uid,
+      author,
+      text,
+    };
+
+    quotesDB.push(quote);
+
+    setAuthor("");
+    setText("");
+  };
+
+  return (
+    <div className="create">
+      <h4>Déposer une citation</h4>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Auteur"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <textarea
+          type="text"
+          placeholder="Citation"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={createQuote}>Create</button>
+      </div>
+    </div>
+  );
+};
+
+export default Create;
